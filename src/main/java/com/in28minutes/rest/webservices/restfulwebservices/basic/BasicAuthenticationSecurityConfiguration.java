323326280eec0c3646140184284e1998c07dd853
vasiioandre <vasi_ioandre@yahoo.com>
2024-08-27 23:44:09 +0300

    Securing the REST API with Spring Security

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.in28minutes.rest.webservices.restfulwebservices.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return
+			http
+				.authorizeHttpRequests(
+					auth -> auth.anyRequest().authenticated()
+					)
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement(
+					session -> session.sessionCreationPolicy
+					(SessionCreationPolicy.STATELESS))
+				.csrf().disable()
+				.build();
+	}
+}
