363c16cb817e943334d64dce6ae5b78141668890
vasiioandre <vasi_ioandre@yahoo.com>
2024-08-28 19:49:52 +0300

    Enable access to preflight request and create a test url for basic athentication

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,7 +16,10 @@ public class BasicAuthenticationSecurityConfiguration {
 		return
 			http
 				.authorizeHttpRequests(
-					auth -> auth.anyRequest().authenticated()
+					auth -> 
+						auth
+						.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+						.anyRequest().authenticated()
 					)
 				.httpBasic(Customizer.withDefaults())
 				.sessionManagement(
